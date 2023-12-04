26c33119e03a7e96d2c496b69187c8f81248af59
7Walter <B00145274@mytudublin.ie>
2023-12-04 22:30:14 +0000

    addedanother sql file and made 2 sql versions

---
+++ createMilitaryServiceDB_Version_1.sql
@@ -0,0 +1,11 @@
+drop database if exists militaryservice;
+create database militaryservice;
+use militaryservice;
+create table Army (
+    id int auto_increment,
+    firstname varchar(30),
+    surname varchar(30),
+    age varchar(2),
+    rank varchar(30),
+    primary key(id)
+);
