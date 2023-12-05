0f43ac9ef4e50d34f0fcc42d6208e489c5504979
7Walter <B00145274@mytudublin.ie>
2023-12-05 23:02:27 +0000

    added the rest of the coloumns to the working table

--- showenlistedsoldiers.php
+++ showenlistedsoldiers.php
@@ -7,8 +7,10 @@ $result = mysqli_query($conn,$sql);
 echo "<TABLE border='1'>";
 while($row = mysqli_fetch_assoc($result)) {
     $fn = $row['firstname'];
-
-    echo "<TR><TD>$fn</TD></TR>";
+    $sn = $row['surname'];
+    $age = $row['age'];
+    $rk = $row['ranking'];
+    echo "<TR><TD>$fn</TD><TD>$sn</TD><TD>$age</TD><TD>$rk</TD></TR>";
 }
 echo "</TABLE>";
 
